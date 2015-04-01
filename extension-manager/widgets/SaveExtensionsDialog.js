e3ce50ec378de105fa88a5d5915dfa4de046c3ea
Salvador Cabrera <sal@genuitec.com>
2015-04-01 10:45:37 -0600

    Displays a "Save Extensions" dialog
    Displays a list of Enabled Extensions in a dialog to review what extensions
    are going to be stored in the manifest file
    
    Change-Id: I5812722c6890893b14b5864c858fc8d0763f944c

--- extension-manager/widgets/SaveExtensionsDialog.js
+++ extension-manager/widgets/SaveExtensionsDialog.js
@@ -7,83 +7,59 @@ define(function (require, exports, module) {
     var SAVE_EXTENSIONS_DIALOG_ID = "save-extensions-dialog",
         SAVE_EXTENSIONS_DIALOG_TITLE = "Save Extensions",
         SAVE_BUTTON_ID = "save-extensions-save-button",
-        SAVE_BUTTON_LABEL = "Save";
+        SAVE_BUTTON_LABEL = "Save",
+        DIALOG_CLASS = "save-extensions-dialog";
     
     function show(installedExtensions) {
-        console.log("Showing extensions dialog");
-//        var 
-//            SaveExtensionsBodyTemplate = require("text!templates/save-extensions-body-dialog.html"),
-        var ExtensionManager = brackets.getModule("extensibility/ExtensionManager"),
+        var SaveExtensionsBodyTemplate = require("text!templates/save-extensions-body-dialog.html"),
+            ExtensionManager = brackets.getModule("extensibility/ExtensionManager"),
             FileSystem = brackets.getModule("filesystem/FileSystem"),
             FileUtils = brackets.getModule("file/FileUtils"),
             ProjectManager = brackets.getModule("project/ProjectManager"),
             Dialogs = brackets.getModule('widgets/Dialogs');
         
-        
-        
-//        var dialogBody = Mustache.render(SaveExtensionsBodyTemplate, {extensions: installedExtensions});
-//
-//        var dialog = Dialogs.showModalDialog(
-//            SAVE_EXTENSIONS_DIALOG_ID,
-//            SAVE_EXTENSIONS_DIALOG_TITLE,
-//            dialogBody,
-//            [
-//                {
-//                    className: Dialogs.DIALOG_BTN_CLASS_PRIMARY,
-//                    id: SAVE_BUTTON_ID,
-//                    text: SAVE_BUTTON_LABEL
-//                },
-//                {
-//                    className: Dialogs.DIALOG_BTN_CANCEL,
-//                    id: Dialogs.DIALOG_BTN_CANCEL,
-//                    text: Dialogs.DIALOG_BTN_CANCEL
-//                }
-//            ],
-//            false
-//        );
-//        var $dlg = dialog.getElement();
-//
-//        $dlg.one("buttonClick", function (e, id) {
-//            console.log(id);
-//            if (id === SAVE_BUTTON_ID) {
-//                $dlg.find('.install-status').append('<h4>Saving...</h4>');
-//
-//                var InstallExtensionDialog = brackets.getModule('extensibility/InstallExtensionDialog');
-//                var missingExtensionsCounter = 0;
-//
-//                missingExtensions.forEach(
-//                    function (missingExtension) {
-//                        Package.installFromURL(
-//                            ExtensionManager.getExtensionURL(missingExtension.id, missingExtension.version)
-//                        ).promise.then(
-//                            function (missingExtension) {
-//                                ++missingExtensionsCounter;
-//                                console.log(missingExtension.id, "installed");
-//                                if (missingExtensionsCounter >= missingExtensions.length) {
-//                                    dialog.close();
-//                                }
-//                            }
-//                        );
-//                    }
-//                );
-//            } else {
-//                dialog.close();
-//            }
-//        });
-        var deferred = $.Deferred();
-        var baseURL = ProjectManager.getProjectRoot().fullPath;
-        var filePath = baseURL + '.team.extensions';
-        
-        var file = FileSystem.getFileForPath(filePath);
-        var fileContents = JSON.stringify(installedExtensions, null, 4);
-        FileUtils.writeText(file, fileContents, true).then(
-            function () {
-                deferred.resolve();
-            },
-            function () {
-                console.log("Error while writing extension");
-            }
+        var dialog = Dialogs.showModalDialog(
+            DIALOG_CLASS,
+            SAVE_EXTENSIONS_DIALOG_TITLE,
+            Mustache.render(SaveExtensionsBodyTemplate, {extensions: installedExtensions}),
+            [
+                {
+                    className: Dialogs.DIALOG_BTN_CLASS_PRIMARY,
+                    id: SAVE_BUTTON_ID,
+                    text: SAVE_BUTTON_LABEL
+                },
+                {
+                    className: Dialogs.DIALOG_BTN_CANCEL,
+                    id: Dialogs.DIALOG_BTN_CANCEL,
+                    text: Dialogs.DIALOG_BTN_CANCEL
+                }
+            ],
+            false
         );
+        
+        var $dlg = dialog.getElement();
+        var deferred = $.Deferred();
+
+        $dlg.one("buttonClick", function (e, id) {
+            if (id === SAVE_BUTTON_ID) {
+                
+                var baseURL = ProjectManager.getProjectRoot().fullPath;
+                var filePath = baseURL + '.team.extensions';
+
+                var file = FileSystem.getFileForPath(filePath);
+                var fileContents = JSON.stringify(installedExtensions, null, 4);
+                FileUtils.writeText(file, fileContents, true).then(
+                    function () {
+                        deferred.resolve();
+                    },
+                    function () {
+                        console.log("Error while writing extensions");
+                    }
+                );
+            } else {
+                dialog.close();
+            }
+        });
         
         return deferred.promise;
     }
